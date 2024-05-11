c76ac2c44f44e97d71e45a3a970a392a7fba71c6
Ashray-Chaitanya <142154081+Ashray-Chaitanya@users.noreply.github.com>
2024-05-11 15:36:13 +0530

    Delete 4a.py

--- 4a.py
+++
@@ -1,8 +0,0 @@
-import matplotlib.pyplot as plt
-categories = ['Category A', 'Category B', 'Category C', 'Category D']
-values = [25, 50, 75, 100]
-plt.bar(categories, values, color='blue')
-plt.xlabel('Categories')
-plt.ylabel('Values')
-plt.title('Bar Plot Example')
-plt.show()
