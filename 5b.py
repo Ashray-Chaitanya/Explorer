2eb270a40881d978134d10673ea5be9e84480fa1
Ashray-Chaitanya <142154081+Ashray-Chaitanya@users.noreply.github.com>
2024-05-11 15:36:22 +0530

    Delete 5b.py

--- 5b.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plt
-
-labels = ['Category A', 'Category B', 'Category C', 'Category D']
-sizes = [30, 45, 15, 10]
-colors = ['lightblue', 'lightcoral', 'lightgreen', 'lightsalmon']
-explode = (0.1, 0, 0, 0) 
-plt.pie(sizes, explode=explode, labels=labels, colors=colors,autopct='%1.1f%%', shadow=True, startangle=140)
-plt.title('Pie Chart Example')
-plt.show()
